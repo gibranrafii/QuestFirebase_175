29ac39c14634a7d88846228e3c36d26f32a439d9
gibranrafii <gibranrafi21@gmail.com>
2026-01-09 14:43:10 +0700

    memanggil edit SiswaScreen untuk memanggil HalamanEdit.kt

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -58,6 +58,10 @@ fun HostNavigasi(
                 navArgument(DestinasiEdit.itemIdArg) { type = NavType.StringType }
             )
         ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
+            )
         }
     }
 }
