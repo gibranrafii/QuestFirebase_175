fbf7d6aafbfd574995f1811f5b0afa919d75cc28
gibranrafii <gibranrafi21@gmail.com>
2026-01-09 14:26:33 +0700

    membuat class viewmodel edit

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,3 +1,22 @@
 package com.example.myfirebase.viewmodel
 
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailsSiswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import com.example.myfirebase.modeldata.toUiStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
+RepositorySiswa
+): ViewModel() {
+
+}
